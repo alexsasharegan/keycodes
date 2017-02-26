0036b92b8a6b25764b23a26b228b83be39e2a893
Alex Regan <alex.regan@artisancolour.com>
2017-02-26 13:00:03 -0700

    basically forked from https://github.com/timoxley/keycode

--- readme.md
+++ readme.md
@@ -1,34 +1,68 @@
 # Keycodes
-A set of named constants mapping to event keycodes.
+Started as my own simple map of named keycode constants, but evolved to my preferred semantics of this well done package: **[timoxley/keycode](https://github.com/timoxley/keycode)**. Now it's not much more than a personal fork of this package.
 
+
+## Installation
+
+#### npm
+
+```sh
+npm install @alexsasharegan/keycodes
+```
+
+## Example
 
 ```js
-const KEYCODES = require('@alexsasharegan/keycodes');
-// {
-// 	BACKSPACE: 8,
-// 	TAB: 9,
-// 	CLEAR: 12,
-// 	RETURN: 13,
-// 	ENTER: 13,
-// 	SHIFT: 16,
-// 	CONTROL: 17,
-// 	ALT: 18,
-// 	OPTION: 18,
-// 	CAPS_LOCK: 20,
-// 	ESCAPE: 27,
-// 	SPACE_BAR: 32,
-// 	PAGE_UP: 33,
-// 	PAGE_DOWN: 34,
-// 	END: 35,
-// 	HOME: 36,
-// 	LEFT_ARROW: 37,
-// 	UP_ARROW: 38,
-// 	RIGHT_ARROW: 39,
-// 	DOWN_ARROW: 40,
-// 	DELETE: 46,
-// 	COMMAND_LEFT: 91,
-// 	WINDOWS: 91,
-// 	COMMAND_RIGHT: 93,
-// 	WINDOWS_MENU: 93,
-// };
+const keycode = require('@alexsasharegan/keycodes');
+
+document.addEventListener('keydown', e => console.log(`You pressed: ${keycode(e)}`) );
 ```
+
+## API
+
+`keycode` tries to make an intelligent guess as to what
+you're trying to discover based on the type of argument
+you supply.
+
+##keycode(keycode:Event)
+
+Returns the name of the key associated with this event.
+
+```js
+document.addEventListener('keyup', e => console.log(keycode(e)) ); // prints name of key
+```
+
+[Due to the keypress event being weird](https://github.com/timoxley/keycode/wiki/wtf%3F-keydown,-keyup-vs-keypress),`keycodes `currently does not support the `keypress` event, but this should not be an issue as `keydown` and `keyup` work perfectly fine.
+
+##keycode(keycode:Number)
+
+Returns the constant name of a given numeric keycode.
+
+```js
+keycode(13); // => 'ENTER'
+keycode(37); // => 'LEFT_ARROW'
+```
+
+##keycode(name:String)
+
+Returns the numeric keycode for given key name.
+
+```js
+keycode('Enter'); // => 13
+
+// keycode is not case sensitive
+keycode('eNtEr'); // => 13
+```
+
+
+## Maps
+
+Key code/name maps are available directly as `keycode.codes` and `keycode.names` respectively.
+
+```js
+keycode.names[13]; // => 'enter'
+keycode.codes.ENTER; // => 13
+```
+
+## Credit
+[timoxley/keycode](https://github.com/timoxley/keycode)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 (function () {
 	'use strict';
 	
-	const KEYCODES = {
+	const numToStrMap = {
+		0: 'ZERO',
+		1: 'ONE',
+		2: 'TWO',
+		3: 'THREE',
+		4: 'FOUR',
+		5: 'FIVE',
+		6: 'SIX',
+		7: 'SEVEN',
+		8: 'EIGHT',
+		9: 'NINE',
+	};
+	
+	const NAMES = {
 		BACKSPACE: 8,
 		TAB: 9,
 		CLEAR: 12,
@@ -11,6 +24,7 @@
 		CONTROL: 17,
 		ALT: 18,
 		OPTION: 18,
+		PAUSE_BREAK: 19,
 		CAPS_LOCK: 20,
 		ESCAPE: 27,
 		SPACE_BAR: 32,
@@ -22,20 +36,84 @@
 		UP_ARROW: 38,
 		RIGHT_ARROW: 39,
 		DOWN_ARROW: 40,
+		INSERT: 45,
 		DELETE: 46,
 		COMMAND_LEFT: 91,
 		WINDOWS: 91,
 		COMMAND_RIGHT: 93,
 		WINDOWS_MENU: 93,
+		NUMPAD_ASTERISK: 106,
+		NUMPAD_PLUS: 107,
+		NUMPAD_DASH: 109,
+		NUMPAD_MINUS: 109,
+		NUMPAD_HYPHEN: 109,
+		NUMPAD_PERIOD: 110,
+		NUMPAD_FORWARD_SLASH: 111,
+		NUM_LOCK: 144,
+		SCROLL_LOCK: 145,
+		MY_COMPUTER: 182,
+		MY_CALCULATOR: 183,
+		SEMICOLON: 186,
+		EQUALS: 187,
+		COMMA: 188,
+		DASH: 189,
+		MINUS: 189,
+		HYPHEN: 189,
+		PERIOD: 190,
+		FORWARD_SLASH: 191,
+		BACKTICK: 192,
+		LEFT_BRACKET: 219,
+		BACKSLASH: 220,
+		RIGHT_BRACKET: 221,
+		APOSTROPHE: 222
 	};
 	
-	if ( typeof module === "object" && typeof module.exports === "object" ) {
-		module.exports = KEYCODES;
-	} else {
-		if ( typeof window !== "undefined" ) {
-			window.KEYCODES = KEYCODES;
+	// letters
+	for ( let i = 97; i < 123; i++ ) NAMES[ `LETTER_${String.fromCharCode( i - 32 )}` ] = i - 32;
+	
+	// function keys
+	for ( let i = 1; i <= 19; i++ ) NAMES[ `F${i}` ] = i + 111;
+	
+	// numbers
+	for ( let i = 0; i <= 9; i++ ) NAMES[ numToStrMap[ i ] ] = i + 48;
+	
+	// numeric keypad keys
+	for ( let i = 0; i < 10; i++ ) NAMES[ `NUMPAD_${numToStrMap[ i ]}` ] = i + 96;
+	
+	// reverse mapping
+	const CODES = {};
+	for ( let name in NAMES ) CODES[ NAMES[ name ] ] = name;
+	
+	const keycode = function ( searchInput ) {
+		// Normalize from event object
+		if ( searchInput && typeof searchInput === 'object' ) {
+			let hasKeyCode = searchInput.which || searchInput.keyCode || searchInput.charCode;
+			if ( hasKeyCode ) searchInput = hasKeyCode
 		}
 		
-		return KEYCODES;
+		// Numbers
+		if ( typeof searchInput === 'number' ) return CODES[ searchInput ];
+		
+		// Everything else (cast to string)
+		let search = String( searchInput );
+		
+		// check codes
+		let foundNamedKey = NAMES[ search.toUpperCase() ];
+		if ( foundNamedKey ) return foundNamedKey;
+		
+		return undefined
+	};
+	
+	keycode.names = NAMES;
+	keycode.codes = CODES;
+	
+	if ( typeof module === "object" && typeof module.exports === "object" ) {
+		module.exports = keycode;
+	} else {
+		if ( typeof window !== "undefined" ) {
+			window.keycode = keycode;
+		} else {
+			return keycode;
+		}
 	}
 }());
